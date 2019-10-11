2f0670540f7e0971c1ff11ed8278c0938319242f
Farry <newfarry@126.com>
2019-10-12 00:46:13 +0800

    init

---
+++ cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	nettool "git.iotserv.com/iotserv/utils/net"
+	"golang.org/x/net/webdav"
+	"net/http"
+)
+
+func main() {
+	var txtInfo = nettool.MDNSServiceBaseInfo
+	txtInfo["name"] = "WebDAV文件服务器"
+	txtInfo["model"] = "com.iotserv.devices.webdav"
+	port, err := nettool.GetOneFreeTcpPort()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	server, err := nettool.RegistermDNSService(txtInfo, port)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), &webdav.Handler{
+		FileSystem: webdav.Dir("."),
+		LockSystem: webdav.NewMemLS(),
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+		server.Shutdown()
+		return
+	}
+}
